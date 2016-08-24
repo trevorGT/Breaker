86677881e4e66f606b9c15e6a95c6dc27d0ae6a7
trevorjiang <trevorjiang@trevorjiangiMac1.local>
2016-08-24 22:10:39 +0800

    [ADD]添加平板的简单移动

---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaddleController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate()
+	{
+		float h = Input.GetAxis ("Horizontal");
+		h *= 5;
+		GetComponent<Rigidbody2D> ().velocity = new Vector2(h, 0);
+	}
+}

56f2c88b4f854521dcfffe3b39c41b19a904fb18
trevorjiang <trevorjiang@trevorjiangiMac1.local>
2016-08-29 19:23:49 +0800

    [ADD]添加LOAD UI界面

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,20 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
-	public int TotalScore = 0;
-	public int TotalLive = 3;
+	public int totalScore = 0;
+	public int totalLive = 3;
 
-	static public GameManager instance;
+	private int level;
+	public float levelStartDelay = 2f;
+	private Text levelText;
+	private GameObject levelImage;
+	private bool doingSetup;
 	// Use this for initialization
 	void Start ()
 	{
-	
+		OnLevelIsLoaded (0);
 	}
 	
 	// Update is called once perø frame
 	void Update ()
 	{
 	
+	}
+
+	public void OnLevelIsLoaded(int level)
+	{
+		Debug.Log ("test");
+		level++;
+		levelImage = GameObject.Find ("LevelImage");
+		levelText = GameObject.Find ("LevelText").GetComponent<Text>();
+		levelText.text = "Level " + level;
+		levelImage.SetActive (true);
+		doingSetup = true;
+
+		Invoke ("HideLevelImage", levelStartDelay);
+	}
+
+	private void HideLevelImage()
+	{
+		levelImage.SetActive (false);
+		doingSetup = false;
 	}
 }

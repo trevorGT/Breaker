80118c247eef07b81a21a7d482ebd67dd8a2fba4
trevorjiang <jgt019@gmail.com>
2016-09-04 23:52:25 +0800

    [ADD]添加彩色砖块和弹球逻辑

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -13,15 +13,23 @@ public class BallController : MonoBehaviour
 {
 	public int damage;
 	private GameColor color;
+    private Rigidbody2D rgb2D;
+    private Vector2 ballVelocity;
 	// Use this for initialization
-	void Start () {
-		
-	}
+	void Awake ()
+    {
+        rgb2D = GetComponent<Rigidbody2D>();
+    }
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	void FixedUpdate()
+    {
+        if (rgb2D.velocity != ballVelocity)
+        {
+            ballVelocity = rgb2D.velocity;
+            print(ballVelocity);
+        }
+    }
 
 	public GameColor Color
 	{
@@ -32,4 +40,38 @@ public class BallController : MonoBehaviour
 			GetComponent<SpriteRenderer>().color = UnityEngine.Color.green;
 		}
 	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        foreach (ContactPoint2D contact in col.contacts)
+        {
+            rgb2D.velocity = GetBounceVelocity(contact.normal, ballVelocity);
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+        print("OnCollisionExit2D");
+    }
+
+    Vector2 GetBounceVelocity(Vector2 normal, Vector2 velocity)
+    {
+        float bounceVelocityX, bounceVelocityY;
+        bounceVelocityX = velocity.x;
+        bounceVelocityY = velocity.y;
+
+        if (normal.x != 0)
+        {
+            bounceVelocityX = velocity.x * -1 + normal.x * 0.5f;
+        }
+
+        if (normal.y != 0)
+        {
+            bounceVelocityY = velocity.y * -1 + normal.y * 0.5f;
+        }
+
+        return new Vector2(bounceVelocityX, bounceVelocityY);
+    }
+
+    
 }

a2fd7cfb96b7f0186b5a2a10c27c2b69f8f61b72
trevorjiang <jgt019@gmail.com>
2016-11-30 19:50:41 +0800

    [add]添加线性插值加速度

--- Assets/Scripts/StrangeIoC/GameContext/View/BallView.cs
+++ Assets/Scripts/StrangeIoC/GameContext/View/BallView.cs
@@ -12,6 +12,12 @@ namespace Game
         [SerializeField]
         private Vector2 _velocity = new Vector2(0, 1);
 
+        private float _speed = 1f;
+        private float lastSpeed;
+
+        private bool isAccelerate = false;
+        private bool isDecelerate = false;
+
         public Vector2 velocity
         {
             get { return _velocity; }
@@ -22,17 +28,46 @@ namespace Game
             }
         }
 
+        public float speed
+        {
+            get { return _speed; }
+            set
+            {
+                _speed = value;
+                rgb2D.velocity = velocity * _speed;
+            }
+        }
+
         protected override void Start()
         {
             base.Start();
             velocity = _velocity;
         }
-
+        
         void Update()
         {
-            Debug.Log("rgb2D velocity:" + rgb2D.velocity);
+            if (isAccelerate)
+            {
+                speed = Mathf.Lerp(speed, 2.5f, Time.deltaTime);
+                Debug.Log(speed);
+                //                 if (speed.Equals(2.5f))
+                //                 {
+                //                     isAccelerate = false;
+                //                     isDecelerate = true;
+                //                 }
+            }
+//             else if (isDecelerate)
+//             {
+//                 speed = Mathf.Lerp(speed, 1f, Time.time);
+// 
+//                 if (speed.Equals(1f))
+//                 {
+//                     isDecelerate = false;
+//                 }
+//             }
+            
         }
-        
+
         void OnCollisionEnter2D(Collision2D col)
         {
             foreach (ContactPoint2D contact in col.contacts)
@@ -45,14 +80,12 @@ namespace Game
                 n *= tempSProductC / tempModeN;
 
                 Vector2 f = 2 * n + s;
-                //f *= 1.1f;
 
                 if (contact.collider.gameObject.tag == "Paddle")
                 {
                     f += new Vector2(.5f, 0);
                 }
-                Debug.Log("velocity:" + f);
-                
+                isAccelerate = true;
                 velocity = f;
             }
         }
